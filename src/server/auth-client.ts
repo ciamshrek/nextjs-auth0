9029930d785da6366c996b472e49deeea7fac2eb
guabu <135956181+guabu@users.noreply.github.com>
2024-12-20 06:44:43 +0100

    4.0.0-beta.13 (#1850)

--- src/server/auth-client.ts
+++ src/server/auth-client.ts
@@ -25,7 +25,8 @@ import { TransactionState, TransactionStore } from "./transaction-store"
 import { filterClaims } from "./user"
 
 export type BeforeSessionSavedHook = (
-  session: SessionData
+  session: SessionData,
+  idToken: string | null
 ) => Promise<SessionData>
 
 type OnCallbackContext = {
@@ -131,7 +132,7 @@ export class AuthClient {
   private clientSecret?: string
   private clientAssertionSigningKey?: string | CryptoKey
   private clientAssertionSigningAlg: string
-  private issuer: string
+  private domain: string
   private authorizationParameters: AuthorizationParameters
   private pushedAuthorizationRequests: boolean
 
@@ -164,11 +165,7 @@ export class AuthClient {
     this.sessionStore = options.sessionStore
 
     // authorization server
-    this.issuer =
-      options.domain.startsWith("http://") ||
-      options.domain.startsWith("https://")
-        ? options.domain
-        : `https://${options.domain}`
+    this.domain = options.domain
     this.clientMetadata = { client_id: options.clientId }
     this.clientSecret = options.clientSecret
     this.authorizationParameters = options.authorizationParameters || {
@@ -241,20 +238,10 @@ export class AuthClient {
       const session = await this.sessionStore.get(req.cookies)
 
       if (session) {
-        // refresh the access token, if necessary
-        const [error, updatedTokenSet] = await this.getTokenSet(
-          session.tokenSet
-        )
-
-        if (error) {
-          return res
-        }
-
         // we pass the existing session (containing an `createdAt` timestamp) to the set method
         // which will update the cookie's `maxAge` property based on the `createdAt` time
         await this.sessionStore.set(req.cookies, res.cookies, {
           ...session,
-          tokenSet: updatedTokenSet,
         })
       }
 
@@ -469,8 +456,14 @@ export class AuthClient {
     const res = await this.onCallback(null, onCallbackCtx, session)
 
     if (this.beforeSessionSaved) {
-      const { user } = await this.beforeSessionSaved(session)
-      session.user = user || {}
+      const updatedSession = await this.beforeSessionSaved(
+        session,
+        oidcRes.id_token ?? null
+      )
+      session = {
+        ...updatedSession,
+        internal: session.internal,
+      }
     } else {
       session.user = filterClaims(idTokenClaims)
     }
@@ -894,6 +887,13 @@ export class AuthClient {
     return clientPrivateKey
       ? oauth.PrivateKeyJwt(clientPrivateKey)
       : oauth.ClientSecretPost(this.clientSecret!)
+  }
+
+  private get issuer(): string {
+    return this.domain.startsWith("http://") ||
+      this.domain.startsWith("https://")
+      ? this.domain
+      : `https://${this.domain}`
   }
 
   /**
